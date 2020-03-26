148b79380c74f3205b3e985a9ec97815f248fd17
hootinator <al851lop@htwg-konstanz.de>
2020-03-26 14:44:01 +0100

    Add project files.

---
+++ TrackRelator/Release.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackRelator
+{
+    class Release
+    {
+        public string Name { get; set; }
+        public string Label { get; set; }
+        public Track[] Tracks { get; set; }
+    }
+}
---
+++ TrackRelator/Track.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackRelator
+{
+    class Track : Vinyl
+    {
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public string Side { get; set; }
+    }
+}
